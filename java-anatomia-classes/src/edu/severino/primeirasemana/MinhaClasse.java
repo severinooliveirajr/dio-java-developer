539befdbf689b5ad5c506295e831eaaf300e32e4
Severino Oliveira <severino.oliveirajr@gmail.com>
2022-08-19 23:21:17 -0300

    Primeiros códigos.

---
+++ java-anatomia-classes/src/edu/severino/primeirasemana/MinhaClasse.java
@@ -0,0 +1,18 @@
+package edu.severino.primeirasemana;
+public class MinhaClasse {
+    
+    public static void main (String [] args) {
+
+        String primeiroNome = "Severino";
+        String segundoNome = "Oliveira";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Nome Complento: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+}
