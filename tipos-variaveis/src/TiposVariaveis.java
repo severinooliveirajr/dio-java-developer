bf57a88fecb00e64dbbd4c9ad2200e66fb05b6fe
Severino Oliveira <severino.oliveirajr@gmail.com>
2022-08-22 07:35:01 -0300

    Final do tópico

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        // VALOR_DE_PI = 3.75; // Não é possível alterar uma variável "final"
+
+        
     }
 }
